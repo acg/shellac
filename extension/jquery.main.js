110d6e24c33dc7ffc417ea6c1d57ffb371ba12f4
Alan Grow <alangrow@gmail.com>
2011-03-18 13:57:33 -0400

    add server status and registered commands to popup

--- extension/jquery.main.js
+++ extension/jquery.main.js
@@ -3,9 +3,27 @@
 var extension_name = "Shellac";
 var base_url = "http://127.0.0.1:8783";
 var actions = {};
+var parent_menu;
 
 
 $(document).ready(function() {
+
+  var uri = window.location.pathname;
+
+  if (uri == '/background.html')
+    init_background();
+  else if (uri == '/popup.html')
+    init_popup();
+
+});
+
+
+function init_background()
+{
+  parent_menu = chrome.contextMenus.create({
+    title: extension_name,
+    contexts: ['all']
+  });
 
   ajax('/config/', {}, {
     type: 'GET',
@@ -14,17 +32,34 @@ $(document).ready(function() {
       setup(data);
     }
   });
+}
 
-});
+
+function init_popup()
+{
+  $('#server').html(base_url);
+  $('#server').attr('href',base_url);
+
+  ajax('/config/', {}, {
+    type: 'GET',
+    dataType: 'json',
+    success: function(config) {
+      $('#status').html('running');
+      $.each( config.actions, function(i,action) {
+        var li = $('<li></li>');
+        li.html(action.title)
+        $('#actions').append(li);
+      });
+    },
+    error: function(data) {
+      $('#status').html('error');
+    }
+  });
+}
 
 
 function setup(config)
 {
-  var parent_id = chrome.contextMenus.create({
-    title: extension_name,
-    contexts: ['all']
-  });
-
   var context_onclick = function(info, tab) {
     var action = actions[info.menuItemId];
     var data = { action: action.name };
@@ -34,13 +69,13 @@ function setup(config)
   };
 
   $.each( config.actions, function(i,action) {
-    var child_id = chrome.contextMenus.create({
+    var child_menu = chrome.contextMenus.create({
       title: action.title,
       onclick: context_onclick,
-      parentId: parent_id,
+      parentId: parent_menu,
       contexts: action.contexts
     });
-    actions[child_id] = action;
+    actions[child_menu] = action;
   });
 }
 
